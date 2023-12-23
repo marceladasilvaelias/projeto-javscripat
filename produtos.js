9c65d6664dac5a31388981042fd548c613b5d44b
marceladasilvaelias <marceladasilvaelias1991@gmail.com>
2023-12-23 18:24:23 -0300

    projeto js

---
+++ produtos.js
@@ -0,0 +1,10 @@
+const menuOptions = [
+    { name: 'X-Salada', price: 30, vagan: false, src: './img/xsalada.jpg' },
+    { name: 'X-Bacon', price: 34, vegan: false, src: './img/xsalada.jpg' },
+    { name: 'X-bacon Egg', price: 39, vegan: false, src: './img/xsalada.jpg' },
+    { name: 'x monstruoso', price: 50, vegan: false, src: './img/xsalada.jpg' },
+    { name: 'Big vegano', price: 55, vegan: true, src: './img/xsalada.jpg' },
+    { name: 'X-vegano', price: 45, vegan: true, src: './img/xsalada.jpg' },
+
+
+]
